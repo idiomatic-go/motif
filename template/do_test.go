19b06deeba99e830d2f08f850e486d6e0b52e9a5
idiomatic-go <markbwilson@outlook.com>
2023-02-27 14:34:27 -0600

    Refactoring

--- template/do_test.go
+++ template/do_test.go
@@ -2,14 +2,15 @@ package template
 
 import (
 	"fmt"
+
 	"net/http"
 )
 
 func ExampleDo() {
 	req, _ := http.NewRequest("GET", "https://www.google.com/search?q=test", nil)
-	resp, buf, status := Do[DebugError, []byte, DefaultClient](req)
-	fmt.Printf("test: Do[DebugError,[]byte,DefaultClient](req) -> [status:%v] [buf:%v] [resp:%v]\n", status, len(buf) > 0, resp != nil)
+	resp, buf, status := Do[DebugError, []byte, DefaultExchange](req)
+	fmt.Printf("test: Do[DebugError,[]byte,DefaultExchange](req) -> [status:%v] [buf:%v] [resp:%v]\n", status, len(buf) > 0, resp != nil)
 
 	//Output:
-	//test: Do[DebugError,[]byte,DefaultClient](req) -> [status:OK] [buf:true] [resp:true]
+	//test: Do[DebugError,[]byte,DefaultExchange](req) -> [status:OK] [buf:true] [resp:true]
 }
---
+++ template/exchange.go
@@ -0,0 +1,16 @@
+package template
+
+import (
+	http2 "github.com/idiomatic-go/motif/http"
+	"net/http"
+)
+
+type Exchange interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+type DefaultExchange struct{}
+
+func (DefaultExchange) Do(req *http.Request) (*http.Response, error) {
+	return http2.Do(req)
+}
