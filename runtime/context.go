380f4de2e82c40c84799cd55e700d0d1a950adb3
idiomatic-go <markbwilson@outlook.com>
2023-03-04 07:53:16 -0600

    Refactoring

--- runtime/context.go
+++ runtime/context.go
@@ -2,8 +2,11 @@ package runtime
 
 import (
 	"context"
+	"errors"
 	"github.com/google/uuid"
+	http2 "github.com/idiomatic-go/motif/http"
 	"net/http"
+	"time"
 )
 
 const (
@@ -57,4 +60,75 @@ func ContextRequestId(ctx context.Context) string {
 		return requestId
 	}
 	return ""
+}
+
+type ExchangeContext interface {
+	context.Context
+	http2.Exchange
+	withValue(key, val any) context.Context
+}
+
+type exchangeContext struct {
+	ctx  context.Context
+	exec http2.Exchange
+}
+
+func NewExchangeContext(ctx context.Context, exec http2.Exchange) context.Context {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	return &exchangeContext{ctx: ctx, exec: exec}
+}
+
+func (c *exchangeContext) Deadline() (deadline time.Time, ok bool) {
+	return c.ctx.Deadline()
+}
+
+func (c *exchangeContext) Done() <-chan struct{} {
+	return c.ctx.Done()
+}
+
+func (c *exchangeContext) Err() error {
+	return c.ctx.Err()
+}
+
+func (c *exchangeContext) Value(key any) any {
+	return c.ctx.Value(key)
+}
+
+func (c *exchangeContext) Do(req *http.Request) (*http.Response, error) {
+	if c.exec == nil {
+		return nil, errors.New("invalid argument: Exchange interface is nil")
+	}
+	return c.exec.Do(req)
+}
+
+//func (c *exchangeContext) t() *exchangeContext {
+//	return c
+//}
+
+func (c *exchangeContext) withValue(key, val any) context.Context {
+	c.ctx = context.WithValue(c.ctx, key, val)
+	return c
+}
+
+func ContextWithValue(ctx context.Context, key any, val any) context.Context {
+	if ctx == nil {
+		return nil
+	}
+	if curr, ok := any(ctx).(ExchangeContext); ok {
+		//ctx2 := curr.t()
+		//ctx2.ctx = context.WithValue(ctx2.ctx, key, val)
+		//ctx2.ctx = context.WithValue(ctx2.ctx, key, val)
+		//return ctx2
+		return curr.withValue(key, val)
+	}
+	return ctx
+}
+
+func IsExchangeContext(ctx context.Context) bool {
+	if _, ok := any(ctx).(ExchangeContext); ok {
+		return true
+	}
+	return false
 }
--- runtime/context_test.go
+++ runtime/context_test.go
@@ -2,9 +2,16 @@ package runtime
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	http2 "github.com/idiomatic-go/motif/http"
 	"net/http"
 )
+
+func testDo(req *http.Request) (*http.Response, error) {
+	fmt.Printf("test: testDo() -> \n")
+	return nil, errors.New("test error")
+}
 
 func ExampleContextWithRequestExisting() {
 	ctx := ContextWithRequestId(context.Background(), "123-456-abc")
@@ -40,5 +47,43 @@ func ExampleContextWithRequest() {
 	//test: ContextWithRequest(nil) -> false
 	//test: ContextWithRequest(req) -> true
 	//test: ContextWithRequest(req) -> x-request-id-value
+
+}
+
+func ExampleExchangeContext() {
+	k1 := "1"
+	k2 := "2"
+	k3 := "3"
+	v1 := "value 1"
+	v2 := "value 2"
+	v3 := "value 3"
+
+	do1 := http2.NewExchange(testDo)
+	ctx := NewExchangeContext(nil, do1)
+
+	fmt.Printf("test: IsExchangeContext(ctx) -> %v\n", IsExchangeContext(ctx))
+	fmt.Printf("test: Values() -> [key1:%v] [key2:%v] [key3:%v]\n", ctx.Value(k1), ctx.Value(k2), ctx.Value(k3))
+
+	ctx1 := ContextWithValue(ctx, k1, v1)
+	fmt.Printf("test: IsExchangeContext(ctx1) -> %v\n", IsExchangeContext(ctx1))
+	fmt.Printf("test: Values() -> [key1:%v] [key2:%v] [key3:%v]\n", ctx1.Value(k1), ctx1.Value(k2), ctx1.Value(k3))
+
+	ctx2 := ContextWithValue(ctx, k2, v2)
+	fmt.Printf("test: IsExchangeContext(ctx2) -> %v\n", IsExchangeContext(ctx2))
+	fmt.Printf("test: Values() -> [key1:%v] [key2:%v] [key3:%v]\n", ctx2.Value(k1), ctx2.Value(k2), ctx2.Value(k3))
+
+	ctx3 := ContextWithValue(ctx, k3, v3)
+	fmt.Printf("test: IsExchangeContext(ctx3) -> %v\n", IsExchangeContext(ctx3))
+	fmt.Printf("test: Values() -> [key1:%v] [key2:%v] [key3:%v]\n", ctx3.Value(k1), ctx3.Value(k2), ctx3.Value(k3))
+
+	//Output:
+	//test: IsExchangeContext(ctx) -> true
+	//test: Values() -> [key1:<nil>] [key2:<nil>] [key3:<nil>]
+	//test: IsExchangeContext(ctx1) -> true
+	//test: Values() -> [key1:value 1] [key2:<nil>] [key3:<nil>]
+	//test: IsExchangeContext(ctx2) -> true
+	//test: Values() -> [key1:value 1] [key2:value 2] [key3:<nil>]
+	//test: IsExchangeContext(ctx3) -> true
+	//test: Values() -> [key1:value 1] [key2:value 2] [key3:value 3]
 
 }
